a788808a25ccf89ad37973419f304460b46d3dca
Atharva Kadam <165936212+Atharva00Kadam@users.noreply.github.com>
2024-10-13 22:33:49 +0530

    Update and rename The-Good-Game to The-Good-Game.py

---
+++ The-Good-Game.py
@@ -0,0 +1,99 @@
+print("Hello! The world is blue, figuratively and literally.")
+
+user_input1 = input("So, what is your name, my adventurer? ")
+print("It is a pleasure to meet you "+user_input1+".")
+
+user_input2 = input("Know anything about water conservation?\nType yes or no (follow this for the entire game): ").upper()
+if user_input2 == "YES" or "NO":
+    print("Nice.. My name is Frenzy Farmer. Millions of crops are gone to waste every year."
+          " These are partly due to many reasons..\nAnyways, let us consider you "+user_input1+" are a farmer.\n"
+                                                                                               ""
+                                                                                               ""
+'''
+        ⠀⠀  ⢰⣿⣿⣿⣆⣀⠀⠀⠀⠀⢠⠀⡘⠀⡄⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠞⢻⣿⣿⣿⡏⠉⠉⠀⠀⢀⣾⣠⡇⣸⠃⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣌⣿⣿⡿⠁⠀⠀⠀⠀⠈⠉⣿⡟⠿⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⠿⣿⣿⣿⣿⣿⣶⣄⠀⠀⣠⣾⣿⠃⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠠⣾⣿⡃⠀⣿⣿⣿⣿⣿⠙⢿⣷⣾⡿⣻⡇⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠈⠛⢿⣶⣿⣿⣿⣿⡿⠀⠀⠉⠁⠀⣿⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⣿⣿⣿⣿⡇⠀⠀⠀⠀⢸⡏⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣷⠀⠀⠀⠀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⢻⣿⣿⡆⠀⠀⢰⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡏⠀⣿⣿⣷⠀⠀⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠁⠀⠸⣿⣿⡄⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡟⠀⠀⠀⢿⣿⣧⢠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⠃⠀⠀⠀⠘⣿⣿⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀'''
+          "\nYou see that the crops are not growing well.. ")
+
+question1 = input("What do you think is the reason?\nA. Less Irrigation, Faulty sprinklers & Lack of Insecticides\nB. Sufficient use of insecticides and pesticides: ").upper()
+if question1 == "A":
+
+    question2 = input("Spot on! Now that you have understood the problem, let us put an end to it.\n"
+                     '''                      
+                             )-(
+                            (   )
+                            |   |
+                            |   |
+                            |___|        '''
+                      ""
+                      " What do you want to start working on first?"
+                      "\nA. Irrigation Problems\nB. Pesticides issue?: ").upper()
+    if question2 == "A":
+        question3 = input("Well there are two good solutions to do this. "
+              "One is a short-term solution (A), while the other (B) is a long term one.\nWhich one do you pick?: ").upper()
+
+        if question3 == "A":
+            print("Now whether you put A or B or C or D, I will teach you how to set up one!")
+
+            question4 = input("You are going good so far! Pick a method from the following below."
+                  "\nA. Regular Maintenance: Ensure that existing irrigation systems are well-maintained and free from leaks or blockages."
+                  "\nB. Mulching: Use organic mulch around crops to retain soil moisture and reduce the need for frequent watering."
+                  "\nC. Water-Saving Techniques: Implement techniques like drip irrigation hoses to target water directly to plant roots,"
+                  " minimizing waste.\nD. Rainwater Harvesting: Collect and store rainwater for irrigation during dry periods.: ").upper()
+            if question4 == "A":
+                print("Steps to actually perfectly implement this is:\n1. Have a daily check-up in your farm."
+                      "\n2. If there are any blockages whatsoever, call the plumber, or aid it my using a packet of MSeal or Flex Seal 😁")
+            elif question4 == "B":
+                print("Steps to perfectly implement this:\n1. Mulching uses in orchards, vegetable rows, raised beds, pathways, around trees, in containers, nurseries and greenhouses, depending on your specific goals and requirements."
+                      "\nAround New Plantation: Mulching plants help to establish and strengthen them.\nAfter Rain or Irrigation: Mulching in agriculture helps lock the moisture."
+                      "\nWhen Weed Growth is a Concern: Apply Mulch during this period as much as needed to suppress weeds.")
+            elif question4 == "C":
+                print("Water and nutrients are delivered across the field in pipes called ‘dripelines’ featuring small units known as ‘drippers’."
+                      "\nEach dripper emits drops containing water and fertilizer, "
+                      "resulting in the uniform application of water and nutrients direct to each plant's root zone, across an entire field. ")
+            elif question4 == "D":
+                print("Step 1 – Assess Needs and Regulations"
+                      "\nStep 2 – Design and Install Catchment System"
+                      "\nStep 3 – Select and Install Storage System"
+                      "\nStep 4 – Implement Filtration and Treatment (if needed)"
+                      "\nStep 5 – Maintain and Optimize")
+
+                question5 = input("How are you feeling thus far? Have you learnt any information? Yes (or) No?: ").upper()
+                if question5 == "YES" or "NO":
+                    print("Well done, nevertheless. You have now learnt one aspect of farming. Rerun the program to lean more of these methods!"
+                          "\nAnd by the way.. this program is made to spread awareness of sustainable agricultural development with accordance to SDG (Sustainable Development Goal: 4)!"
+                          "\nThank you for playing this game! Have fun!")
+#Case (insecticides)
+    elif question2 == "B":
+        print("Interesting pick you have chosen.\n"
+              "Lack of insecticides is indeed an issue."
+              "\nWithout the use of pesticides, there would be a 78% loss of fruit production, "
+              "\na 54% loss of vegetable production, and a 32% loss of cereal production. Therefore, "
+              "\npesticides play a critical role in reducing diseases and increasing crop yields worldwide")
+        question0 = input("Alright fine, so I present you 2 options: 1) Spray Biopesticides\n2) Spray pesticides: ")
+        if question0 == "1":
+            print("Well done mate! You can perfectly use these without any issues. Some of these include:"
+                  "Diatomaceous earth (fossilized water microbes)\nNeem oil (a tree oil extract)"
+                  "\nPyrethrins (an extract from chrysanthemums)"
+                  "\nInsecticidal soaps and oils\nBotanical insecticides\nEssential oils")
+            print("Now go back and start with the other option-> The Irrigation!")
+
+        if question0 == "2":
+            print("You could have taken the choice of biopesticides.."
+                  " Well.well.well..\nWear the right protective gear."
+                  "\nAvoid contact with eyes, skin, or clothing."
+                  "\nMix outdoors or in a well-ventilated area."
+                  "\nWash hands after the task.")
+            print("Now go back and start with the other option-> The Irrigation!")
+
