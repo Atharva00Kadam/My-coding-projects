846767b60a206a54febcd74b4ac62f4b0d77e8f9
Atharva Kadam <165936212+Atharva00Kadam@users.noreply.github.com>
2025-04-28 12:17:36 +0530

    Update Password_Generator_1.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -13,16 +13,3 @@ dlo = random.choice(Special)
 
 # Construct the final password correctly
 print("1234" + alo + blo + str(clo) + dlo)
-
-#(or)
-import random
-import string
-
-def generate_password(length=8):
-    characters = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
-    password = "".join(random.choice(characters) for _ in range(length))
-    return password
-
-# Ask the user for the desired password length
-length = int(input("Enter the password length: "))
-print("Generated password:", generate_password(length))
